7178b25df26f38b0b7023ce8981cd80ffbf0d2d4
NandoFilter <fernandohfilter10@gmail.com>
2021-10-12 12:07:06 -0300

    Create aula11 files

---
+++ Courses/JavaScript/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 90
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 70) {
+    console.log("Você ultrapassou o limite de velocidade!")
+}
---
+++ Courses/JavaScript/aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'Brasil'
+
+console.log(`Nasci em ${pais}`)
+
+if(pais == 'Brasil'){
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é Estrangeiro!')
+}
