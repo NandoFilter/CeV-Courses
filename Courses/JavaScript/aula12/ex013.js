5dcddadc1d55cc494f5dea0d5cc55698388c384a
NandoFilter <fernandohfilter10@gmail.com>
2021-10-16 20:07:13 -0300

    Update ex013.js

--- Courses/JavaScript/aula12/ex013.js
+++ Courses/JavaScript/aula12/ex013.js
@@ -6,23 +6,23 @@ switch (dia) {
         break
 
     case 1:
-        console.log('Segunda')
+        console.log('Segunda-feira')
         break
 
     case 2:
-        console.log('Terça')
+        console.log('Terça-feira')
         break
 
     case 3:
-        console.log('Quarta')
+        console.log('Quarta-feira')
         break
 
     case 4:
-        console.log('Quinta')
+        console.log('Quinta-feira')
         break
 
     case 5:
-        console.log('Sexta')
+        console.log('Sexta-feira')
         break
 
     case 6:
@@ -32,5 +32,4 @@ switch (dia) {
     default:
         console.log('[ERRO] Dia inválido!')
         break
-
 }
