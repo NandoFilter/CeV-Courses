5718b513a2d25c81944baf6c022eb27f36a34b6d
NandoFilter <fernandohfilter10@gmail.com>
2021-10-17 01:09:09 -0300

    Organizing the code

--- Courses/JavaScript/aula12_ex/ex014/script.js
+++ Courses/JavaScript/aula12_ex/ex014/script.js
@@ -1,4 +1,3 @@
-
 function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
@@ -8,13 +7,19 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if (hora >= 6 && hora < 12) {
+
         img.src = 'img/manha.png'
         document.body.style.background = '#acc0c9'
+
     } else if (hora >= 12 && hora <= 18) {
+
         img.src = 'img/tarde.png'
         document.body.style.background = '#fdc765'
+
     } else {
+
         img.src = 'img/noite.png'
         document.body.style.background = '#7c5477'
+        
     }
 }
