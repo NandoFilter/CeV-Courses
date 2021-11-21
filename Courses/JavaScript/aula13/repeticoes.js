a8ae77a2f3d48c2d890b70650347058221f86bb4
NandoFilter <fernandohfilter10@gmail.com>
2021-11-21 18:09:24 -0300

    Create repeticoes.js

---
+++ Courses/JavaScript/aula13/repeticoes.js
@@ -0,0 +1,19 @@
+var c = 1;
+while ( c <= 5 ) {
+    console.log(`Passo ${c}`)
+    c++;
+}
+
+console.log('--------------------------------')
+
+c = 1;
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while ( c <= 5 )
+
+console.log('--------------------------------')
+
+for ( var c = 1; c <= 5; c++ ) {
+    console.log(`Passo ${c}`)
+}
